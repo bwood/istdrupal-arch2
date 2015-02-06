b26116c10ef667d20603e064e9deb2a8aee8e2d7
Brian Wood <bwood@berkeley.edu>
2015-02-05 16:34:19 -0800

    exec a command works

--- src/Command/NewSiteCommand.php
+++ src/Command/NewSiteCommand.php
@@ -12,16 +12,22 @@ class NewSiteCommand extends Command {
   protected function configure() {
     $this->setName('new-site')
       ->setDescription('Create a new site')
-      ->addOption('site', 's', InputOption::VALUE_REQUIRED, 'Site shortname for the new site');
+      ->addArgument('site', InputArgument::REQUIRED, 'Site short name for the new site');
   }
 
   /**
    * {@inheritDoc}
    */
   protected function execute(InputInterface $input, OutputInterface $output) {
-    $site_name = $input->getOption('site');
-    $terminus_sites_create_cmd = "terminus sites create --product=\$product_uuid --name=$site_name --label=$site_name --org=\$organization_test_uuid";
+    $site_name = $input->getArgument('site');
+    //$terminus_sites_create_cmd = "terminus sites create --product=\$product_uuid --name=$site_name --label=$site_name --org=\$organization_test_uuid";
+    $terminus_sites_create_cmd = "terminus site info --site=$site_name";
     $output->writeln($terminus_sites_create_cmd);
+    exec($terminus_sites_create_cmd, $out, $return);
+    if ($return !== 0) {
+      $output->writeln("Error in last command");
+    }
+    $output->writeln($out);
 
   }
 }
